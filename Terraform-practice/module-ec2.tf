557a33245d672e0e736c5e7ed12c45e6d6b3ecff
Alekhya Kandregula <143278367+alekhya-510@users.noreply.github.com>
2025-02-21 19:14:01 +0000

    Create module-ec2.tf

---
+++ Terraform-practice/module-ec2.tf
@@ -0,0 +1,12 @@
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-053b0d53c279acc90"
+  instance_type_value = "t2.micro"
+}
+output "public_ip_address" {
+  value = module.ec2_instance.public_ip
+}
+output "private_ip_address" {
+  value = module.ec2_instance.private_ip
+}
