3002fa9c68a0700b90a7d0d121e55933fd5a6f98
Alekhya Kandregula <143278367+alekhya-510@users.noreply.github.com>
2025-02-23 08:21:22 +0000

    Update module-ec2.tf

--- Terraform-practice/module-ec2.tf
+++ Terraform-practice/module-ec2.tf
@@ -1,7 +1,7 @@
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90"
+  source = "./modules/ec2_instance"    // path to the source file
+  ami_value = "ami-053b0d53c279acc90"  
   instance_type_value = "t2.micro"
 }
 output "public_ip_address" {
